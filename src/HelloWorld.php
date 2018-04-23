473c578f034f88f17286c4849a3e4dfd80f34bcb
Leonardo Cidral <lcidral@gmail.com>
2018-04-23 06:56:49 -0300

    sources described in composer.json

---
+++ src/HelloWorld.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace lcidral\developstack;
+
+
+class HelloWorld
+{
+    private $hello = "Hello";
+    private $world = "World";
+
+    /**
+     * @return string
+     */
+    public function getHello(): string
+    {
+        return $this->hello;
+    }
+
+    /**
+     * @return string
+     */
+    public function getWorld(): string
+    {
+        return $this->world;
+    }
+
+    public function getHelloWorld() {
+        return $this->hello.' '.$this->world;
+    }
+}
