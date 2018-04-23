aee744c04c6bc19c36f65e59e6b294e302b575e4
Leonardo Cidral <lcidral@gmail.com>
2018-04-23 18:17:44 -0300

    added new test

--- tests/ApiCest.php
+++ tests/ApiCest.php
@@ -1,10 +1,18 @@
 <?php
-class ApiCest 
-{    
+class ApiCest
+{
     public function tryApi(ApiTester $I)
     {
         $I->sendGET('/');
         $I->seeResponseCodeIs(200);
         $I->seeResponseIsJson();
+    }
+
+    public function tryHello(ApiTester $I)
+    {
+        $I->sendGET('/hello');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->seeResponseContains('Hello World');
     }
 }
