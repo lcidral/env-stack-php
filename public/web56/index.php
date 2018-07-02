9862b2ad036f1790a336da0d0d01cced38c8a3c7
Leonardo Cidral <lcidral@gmail.com>
2018-07-02 12:31:24 -0300

    php56

--- public/web56/index.php
+++ public/web56/index.php
@@ -1,7 +1,2 @@
 <?php
-require __DIR__ . '/../../vendor/autoload.php';
-
-define('C3_CODECOVERAGE_ERROR_LOG_FILE', '/logs/c3_error.log'); //Optional (if not set the default c3 output dir will be used)
-include __DIR__ . '/../../c3.php';
-
 phpinfo();
