23fde440dfffe7d960d19990806f305c6ab822eb
Leonardo Cidral <lcidral@gmail.com>
2018-07-25 17:05:37 -0300

    test with private method, using mockery

--- tests/unit/HelloWordTest.php
+++ tests/unit/HelloWordTest.php
@@ -2,10 +2,12 @@
 
 namespace Tests;
 
+use Mockery;
 use lcidral\developstack\HelloWorld;
 
 class HelloWordTest extends \PHPUnit\Framework\TestCase
 {
+
     function testHello()
     {
         $hello = (new HelloWorld())->getHello();
@@ -22,5 +24,25 @@ class HelloWordTest extends \PHPUnit\Framework\TestCase
     {
         $helloWorld = (new HelloWorld())->getHelloWorld();
         $this->assertEquals('Hello World', $helloWorld);
+    }
+
+    public function testShouldHaveHello()
+    {
+        $mock = Mockery::mock('lcidral\developstack\HelloWorld');
+        $mock->shouldReceive('getHello')->once()->andReturn('Hello');
+
+        $this->assertEquals('Hello', $mock->getHello());
+    }
+
+    public function testShouldHaveWackosFactory() {
+
+        $mock = Mockery::mock('lcidral\developstack\HelloWorld')
+            ->shouldAllowMockingProtectedMethods();
+
+        $mock->shouldReceive('getWackos')
+            ->once()
+            ->andReturn('Wackos Factory');
+
+        $this->assertEquals('Wackos Factory', $mock->getWackos());
     }
 }
