57c344c7664cf4c0dab1f184d8d1c720c7018f74
Leonardo Cidral <lcidral@gmail.com>
2018-07-27 17:06:18 -0300

    debug

--- conf/slim/routes.php
+++ conf/slim/routes.php
@@ -3,6 +3,8 @@
 use BotMan\BotMan\BotMan;
 use BotMan\BotMan\BotManFactory;
 use BotMan\BotMan\Drivers\DriverManager;
+use FilippoToso\BotMan\Drivers\RocketChat\RocketChatAuth;
+
 
 $app->get('/', function (\Slim\Http\Request $request, \Slim\Http\Response $response, array $args) {
 
@@ -48,24 +50,47 @@ $app->post('/mail', function ($request, $response)
 $app->post('/rocket', function ($request, $response)
 {
 
+    $debug = [];
+
     $parsedBody = $request->getParsedBody();
 
-    $debug = print_r($parsedBody['text'], 1);
+    $debug['parseBody'] = $parsedBody;
 
-    mail("test@example.com","Testing php -v ".phpversion(),$debug);
+    $auth = RocketChatAuth::getAuth(
+        getenv('ROCKETCHAT_USERNAME'),
+        getenv('ROCKETCHAT_PASSWORD'),
+        'http://php-devstack-api_rocketchat_1:3000'
+    );
+
+    $debug['auth'] = $auth;
 
     $config = [
         // Your driver-specific configuration
         "rocketchat" => [
-            //"incomingEndpoint" => getenv('ROCKET_CHAT_INCOMING_ENDPOINT'),
-            "token" => getenv('ROCKET_CHAT_TOKEN')
-        ]
+            "endpoint" => 'http://php-devstack-api_rocketchat_1:3000/hooks/Gj4nbNy4EnAKmCZhD/Hh3r6gBybb56jcACDHJfXfATcBHPath7xJupDkx3S775cp8u',
+            "token" => getenv('ROCKETCHAT_OUTGOING_TOKEN')
+        ],
+        'user_id' =>  getenv('ROCKETCHAT_USER_ID'),
+        'bot' => [
+            'username' => getenv('ROCKETCHAT_USERNAME'),
+            'password' => getenv('ROCKETCHAT_PASSWORD'),
+        ],
+        'matchingKeys' => [
+            'user_id', 'user_name', 'channel_id', 'text', 'message_id', 'timestamp', 'bot',
+        ],
+        //'auth' => $auth
     ];
 
-    print_r($config);
+    $debug['config'] = $config;
 
     DriverManager::loadDriver(\FilippoToso\BotMan\Drivers\RocketChat\RocketChatDriver::class);
     BotManFactory::create($config);
     $botman = BotManFactory::create($config);
-    $botman->say('Message', 'eumgPqfsBxJt76MD9');
+    $response = $botman->say('Hello user', getenv('ROCKETCHAT_USER_ID'), \FilippoToso\BotMan\Drivers\RocketChat\RocketChatDriver::class);
+
+    $debug['botman'] = $botman;
+    $debug['response'] = $response;
+
+    $debug = print_r($debug, 1);
+    mail("test@example.com","Testing php -v ".phpversion(),$debug);
 });
